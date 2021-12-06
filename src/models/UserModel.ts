f7821edf1ca5f58a1c02bfb8d7674a5737199ad4
Amom Augusto <amom_augusto@hotmail.com>
2021-12-06 12:12:10 -0300

    Model criado

---
+++ src/models/UserModel.ts
@@ -0,0 +1,26 @@
+import { DataTypes } from "sequelize/dist";
+import { db } from "../database/db";
+
+export const UserModel = db.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
